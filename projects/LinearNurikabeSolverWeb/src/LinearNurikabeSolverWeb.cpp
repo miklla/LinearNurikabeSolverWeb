d387a7c2564644ddd624e0f170ed24877a5bdd14
mikl2sch@gmail.com <mikl2sch@gmail.com>
2023-12-22 10:59:21 +0300

    11

--- projects/LinearNurikabeSolverWeb/src/LinearNurikabeSolverWeb.cpp
+++ projects/LinearNurikabeSolverWeb/src/LinearNurikabeSolverWeb.cpp
@@ -3,14 +3,23 @@
 
 #include "LinearNurikabeSolverWeb.h"
 #include <ctime>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 int main()
 {
+    high_resolution_clock::time_point start_time = high_resolution_clock::now();
+
     long r = time(NULL);
-    for (long i = 0; i < 1000000; ++i)
+    for (long i = 0; i < 2000000000; ++i)
         r += i;
 	cout << "Hello CMake. v2 " << r << endl;
+    
+    auto int_ms = duration_cast<milliseconds>(high_resolution_clock::now() - start_time);
+
+    cout << ' ' << int_ms.count() << " ms\n";
+
 	return 0;
 }
